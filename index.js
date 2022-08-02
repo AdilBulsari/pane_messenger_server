0b3b71411d8fd765be3f4d11b3302fcb804c2be3
Nicholas Russell <thatnick@me.com>
2022-08-02 16:34:08 +0100

    Add / route

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const { PORT = 9090 } = process.env;
 
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-io.on("connection", (socket) => {
-  socket.on("chat message", ({ user, msg }) => {
-    io.emit("chat message", { user, msg });
-  });
-});
+app.get(
+  "/",
+  io.on("connection", (socket) => {
+    socket.on("chat message", ({ user, msg }) => {
+      io.emit("chat message", { user, msg });
+    });
+  })
+);
 
 server.listen(PORT, () => {
   console.log("listening on *: " + PORT);
