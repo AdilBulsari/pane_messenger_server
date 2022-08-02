c6e177bd2dcf3da800d496c021c8b94803f57022
Nicholas Russell <thatnick@me.com>
2022-08-02 21:56:50 +0100

    Fix CORS error

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { PORT = 9090 } = process.env;
 const cors = require("cors");
+const http = require("http");
+
+const ENV = process.env.NODE_ENV || "development";
+const { PORT = 9090 } = process.env;
+
+const app = express();
+const server = http.createServer(app);
 
 app.use(cors);
 
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ENV === "production" ? "PROD URL HERE" : "http://localhost:3000",
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log("Connected");
   socket.on("chat message", ({ user, msg }) => {
-    console.log("message recieved");
     io.emit("chat message", { user, msg });
   });
 });
 
 server.listen(PORT, () => {
-  console.log("listening on *:" + PORT);
+  console.log("listening on:" + PORT);
 });
