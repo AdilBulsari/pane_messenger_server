49734c2e4895f8b2d7011068b6003639cef83b50
Nicholas Russell <thatnick@me.com>
2022-08-02 16:42:08 +0100

    Fix socket.io dependency

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const { PORT = 9090 } = process.env;
 
 const { Server } = require("socket.io");
 const io = new Server(server);
-app.get(
-  "/",
-  io.on("connection", (socket) => {
-    socket.on("chat message", ({ user, msg }) => {
-      io.emit("chat message", { user, msg });
-    });
-  })
-);
+// app.get(
+//   "/",
+io.on("connection", (socket) => {
+  socket.on("chat message", ({ user, msg }) => {
+    io.emit("chat message", { user, msg });
+  });
+});
+// );
 
 server.listen(PORT, () => {
-  console.log("listening on *: " + PORT);
+  console.log("listening on *:" + PORT);
 });
